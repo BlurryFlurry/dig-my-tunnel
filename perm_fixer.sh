88dc0d292699a29f55ca876d069640528037229b
BlurryFlurry <chan@elakiri.com>
2023-05-12 11:57:09 +0530

    perm fix delete user

--- perm_fixer.sh
+++ perm_fixer.sh
@@ -4,7 +4,7 @@ username=$1
 logfile=/var/log/$username.log
 touch "$logfile"
 chown "$username":"$username" "$logfile"
-echo "$username ALL=(ALL) NOPASSWD:/usr/sbin/reboot, /usr/sbin/useradd, /usr/bin/tee, /usr/bin/passwd" | sudo tee /etc/sudoers.d/"$username"-commands > /dev/null
+echo "$username ALL=(ALL) NOPASSWD:/usr/sbin/reboot, /usr/sbin/useradd, /usr/bin/tee, /usr/sbin/userdel, /usr/bin/passwd" | sudo tee /etc/sudoers.d/"$username"-commands > /dev/null
 setfacl -m u:"$username":r /etc/shadow
 
 systemctl restart ptb@"$username".service
